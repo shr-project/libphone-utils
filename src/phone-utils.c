9bfbc8048589b2f79bf48f6df6102039076fb491
root <root@tompc.(none)>
2009-07-18 12:58:23 +0300

    phone_utils_user_get* now alloc data on return phone_utils_save_config_to_file was implemented cleaned the code a bit more. whene initializing, if everything went correctly, nothing should be NULL, i.e NULL is not allowed anymore.

--- src/phone-utils.c
+++ src/phone-utils.c
@@ -16,8 +16,8 @@
 
 #include <stdio.h>
 #include <string.h>
-#include <unistd.h>
 #include <stdlib.h>
+#include <errno.h>
 
 #include <glib.h>
 
@@ -31,8 +31,8 @@ static char *national_prefix = NULL;
 static int national_prefix_len = 0;
 static char *country_code = NULL;
 static int country_code_len = 0;
-static char *home_code = NULL;
-static int home_code_len = 0;
+static char *area_code = NULL;
+static int area_code_len = 0;
 
 static char *trailing_delimiters = "wWpP;,";
 static char *possible_chars = "0123456789+";
@@ -47,8 +47,8 @@ phone_utils_init()
 	national_prefix_len = 0;
 	country_code = NULL;
 	country_code_len = 0;
-	home_code = NULL;
-	home_code_len = 0;
+	area_code = NULL;
+	area_code_len = 0;
 
 	/*
 	trailing_delimiters = NULL;
@@ -67,16 +67,16 @@ phone_utils_deinit()
 		free(national_prefix);
 	if (country_code)
 		free(country_code);
-	if (home_code)
-		free(home_code);
+	if (area_code)
+		free(area_code);
 
-	international_prefix = national_prefix = country_code = home_code = NULL;
+	international_prefix = national_prefix = country_code = area_code = NULL;
 }
 
 
 int
 phone_utils_set_codes(char *_international_prefix, char *_national_prefix, 
-                      char *_country_code, char *_home_code)
+                      char *_country_code, char *_area_code)
 {
 	int ret = 0;
 	if (_international_prefix)
@@ -85,8 +85,8 @@ phone_utils_set_codes(char *_international_prefix, char *_national_prefix,
 		ret = ret || phone_utils_set_user_national_prefix(_national_prefix);
 	if (_country_code)
 		ret = ret || phone_utils_set_user_country_code(_country_code);
-	if (_home_code)
-		ret = ret || phone_utils_set_user_home_code (_home_code);
+	if (_area_code)
+		ret = ret || phone_utils_set_user_area_code (_area_code);
 
 	return ret;
 }
@@ -110,63 +110,143 @@ phone_utils_init_from_file(const char *filename)
 		        "Initializing key from file failed\n", __FILE__, __LINE__);
 		return 1;
 	}
-	flags = G_KEY_FILE_KEEP_COMMENTS | G_KEY_FILE_KEEP_TRANSLATIONS;
+	flags = G_KEY_FILE_KEEP_TRANSLATIONS;
 	
 	if (!g_key_file_load_from_file (keyfile, filename, flags, &error)) {
 		g_key_file_free(keyfile);
 		g_warning (error->message);
+		g_error_free(error);
 		return 1;
 	}
 
-	ret = ret || phone_utils_set_user_international_prefix(
-		tmp = g_key_file_get_string(keyfile,"local","international_prefix",NULL));
-	free(tmp);
+	/* Init all the fields */
+	tmp = g_key_file_get_string(keyfile,PHONE_UTILS_CONFIG_CODES_GROUP,
+		                            "international_prefix",NULL);
+	ret = ret || phone_utils_set_user_international_prefix(tmp);
+	g_free(tmp);
 	
-	ret = ret || phone_utils_set_user_national_prefix(
-		tmp = g_key_file_get_string(keyfile,"local","national_prefix",NULL));
-	free(tmp);
+	tmp = g_key_file_get_string(keyfile,PHONE_UTILS_CONFIG_CODES_GROUP,
+		                            "national_prefix",NULL);
+	ret = ret || phone_utils_set_user_national_prefix(tmp);
+	g_free(tmp);
+
+
+	tmp = g_key_file_get_string(keyfile,PHONE_UTILS_CONFIG_CODES_GROUP,
+		                            "country_code",NULL);
+	ret = ret || phone_utils_set_user_country_code(tmp);
+	g_free(tmp);
 	
-	ret = ret || phone_utils_set_user_country_code(
-		tmp = g_key_file_get_string(keyfile,"local","country_code",NULL));
-	free(tmp);
-	
-	ret = ret || phone_utils_set_user_home_code(
-		tmp = g_key_file_get_string(keyfile,"local","home_code",NULL));
-	free(tmp);
+	tmp = g_key_file_get_string(keyfile,PHONE_UTILS_CONFIG_CODES_GROUP,
+		                            "area_code",NULL);
+	ret = ret || phone_utils_set_user_area_code(tmp);
+	g_free(tmp);
 
 	g_key_file_free(keyfile);
 	return ret;
 }
 
 int
+phone_utils_save_config_to_file(const char *filename) 
+{
+	GKeyFile *keyfile;
+	GError *error = NULL;
+	FILE *out;
+	char *data;
+	int ret = 0;
+
+	keyfile = g_key_file_new ();
+	if (!keyfile) {
+		g_debug("Can't allocate memory! %s:%d\n"
+		        "Initializing key from file failed\n", __FILE__, __LINE__);
+		ret = 1;
+		goto end;
+	}
+
+	/* set all the values */
+	{
+		char *tmp;
+		tmp = phone_utils_get_user_international_prefix();
+		g_key_file_set_value(keyfile, PHONE_UTILS_CONFIG_CODES_GROUP, 
+		                     "international_prefix", tmp);
+		free(tmp);
+
+		tmp =  phone_utils_get_user_national_prefix();	
+		g_key_file_set_value(keyfile, PHONE_UTILS_CONFIG_CODES_GROUP, 
+		                     "national_prefix", tmp);
+		free(tmp);
+
+		tmp =  phone_utils_get_user_country_code();
+		g_key_file_set_value(keyfile, PHONE_UTILS_CONFIG_CODES_GROUP, 
+		                     "country_code", tmp);
+		free(tmp);
+
+		tmp =  phone_utils_get_user_area_code();
+		g_key_file_set_value(keyfile, PHONE_UTILS_CONFIG_CODES_GROUP, 
+		                     "area_code", tmp);
+		free(tmp);
+	}
+	
+	if (!(data = g_key_file_to_data (keyfile, NULL, &error))) {
+		g_warning (error->message);
+		g_error_free(error);
+		ret = 1;
+		goto free_key_file;
+	}
+
+	out = fopen(filename, "w");
+	if (!out) {
+		g_debug("%s:%d Can't open file %s! (%s)\n",
+		        __FILE__, __LINE__, filename, strerror(errno));
+		ret = 1;
+		goto free_data;
+	}
+
+	fputs(data, out);
+	if (ferror(out)) {
+		g_debug("%s:%d Can't write to file %s! (%s)\n",
+		        __FILE__, __LINE__, filename, strerror(errno));
+	}
+	
+/*clean up and exit */
+	
+	fclose(out);
+free_data:
+	g_free(data);
+free_key_file:
+	g_key_file_free(keyfile);
+end:
+	return ret;
+}
+
+int
 phone_utils_is_initialized()
 {
-	return international_prefix && national_prefix && country_code && home_code
+	return international_prefix && national_prefix && country_code && area_code
 		&& trailing_delimiters && possible_chars && filler_chars;
 }
 
-const char *
+char *
 phone_utils_get_user_international_prefix() 
 {
-	return international_prefix;
+	return (international_prefix) ? strdup(international_prefix) : NULL;
 }
 
-const char *
+char *
 phone_utils_get_user_national_prefix() 
 {
-	return national_prefix;
+	return (national_prefix) ? strdup(national_prefix) : NULL;
 }
 
-const char *
+char *
 phone_utils_get_user_country_code()
 {
-	return country_code;
+	return (country_code) ? strdup(country_code) : NULL;
 }
 
-const char * 
-phone_utils_get_user_home_code() 
+char * 
+phone_utils_get_user_area_code() 
 {
-	return home_code;
+	return (area_code) ? strdup(area_code) : NULL;
 }
 
 int
@@ -174,7 +254,7 @@ phone_utils_set_user_international_prefix(const char *value)
 {
 	char *tmp;
 	if (!value)
-		return 1;
+		value = "";
 	
 
 	tmp = strdup(value);
@@ -199,7 +279,7 @@ phone_utils_set_user_national_prefix(const char *value)
 {
 	char *tmp;
 	if (!value)
-		return 1;
+		value = "";
 	
 
 	tmp = strdup(value);
@@ -223,7 +303,7 @@ phone_utils_set_user_country_code(const char *value)
 {
 	char *tmp;
 	if (!value)
-		return 1;
+		value = "";
 	
 
 	tmp = strdup(value);
@@ -243,11 +323,11 @@ phone_utils_set_user_country_code(const char *value)
 }
 
 int
-phone_utils_set_user_home_code(const char *value) 
+phone_utils_set_user_area_code(const char *value) 
 {
 	char *tmp;
 	if (!value)
-		return 1;
+		value = "";
 	
 
 	tmp = strdup(value);
@@ -258,11 +338,11 @@ phone_utils_set_user_home_code(const char *value)
 		return 1;
 	}
 	
-	if (home_code) 
-		free(home_code);
+	if (area_code) 
+		free(area_code);
 
-	home_code = tmp;	
-	home_code_len = strlen(home_code);
+	area_code = tmp;	
+	area_code_len = strlen(area_code);
 	return 0;
 }
 
