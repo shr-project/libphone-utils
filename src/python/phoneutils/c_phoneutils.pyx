f82ca77561439fab95ec53a644fc12d116ff8295
Sebastian Krzyszkowiak <seba.dos1@gmail.com>
2009-07-27 18:34:13 +0200

    python-phoneutils: remove unnecessary imports

--- src/python/phoneutils/c_phoneutils.pyx
+++ src/python/phoneutils/c_phoneutils.pyx
@@ -4,7 +4,7 @@
 #
 # python-phoneutils is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as published by
-# the Free Software Foundation, either version 2 of the License, or
+# the Free Software Foundation, either version 2.1 of the License, or
 # (at your option) any later version.
 #
 # python-phoneutils is distributed in the hope that it will be useful,
@@ -16,9 +16,6 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with libphone-utils.  If not, see <http://www.gnu.org/licenses/>.
 #
-
-import sys
-import traceback
 
 cdef extern from "phone-utils.h":
     int phone_utils_init()
